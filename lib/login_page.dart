c7972e193afb753a10426a372cc5c7409b17af31
CalvinVirya <calvinviryayunardy@gmail.com>
2024-12-15 18:29:18 +0700

    versi 2

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,15 +10,174 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset('Assets/Logo.png'),
-          ],
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'Assets/Logo.png',
+                width: screenWidth * 0.5,
+                height: screenHeight * 0.25,
+              ),
+              // EMAIL
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFF353535),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(),
+                    labelText: 'Email',
+                    labelStyle: TextStyle(
+                      color: Color(0xFFB5B5B5),
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    contentPadding: EdgeInsets.only(left: 15),
+                    fillColor: Colors.white,
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: screenHeight * 0.02,
+              ),
+              // PASSWORD
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFF353535),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(),
+                    labelText: 'Password',
+                    labelStyle: TextStyle(
+                      color: Color(0xFFB5B5B5),
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    contentPadding: EdgeInsets.only(left: 15),
+                    fillColor: Colors.white,
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: screenHeight * 0.03,
+              ),
+              // BUTTON LOGIN
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 40),
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text('Log in'),
+              ),
+              SizedBox(
+                height: screenHeight * 0.02,
+              ),
+              Text(
+                'Or Log In With',
+                style: TextStyle(
+                  color: Color(0xFFB5B5B5),
+                ),
+              ),
+              SizedBox(
+                height: screenHeight * 0.02,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 40),
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'Assets/Google.png',
+                      height: 17,
+                      width: 17,
+                    ),
+                    SizedBox(
+                      width: 6,
+                    ),
+                    Text('Continue with Google'),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: screenHeight * 0.02,
+              ),
+              Container(
+                width: double.infinity,
+                child: RichText(
+                  text: TextSpan(
+                    style: TextStyle(color: Colors.white),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text:
+                              'By clicking Continue, you agree to Shadow Space’s '),
+                      TextSpan(
+                        text: 'User Agreement',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(text: ', '),
+                      TextSpan(
+                        text: 'Privacy Policy',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(text: ', and '),
+                      TextSpan(
+                        text: 'Cookie Policy',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(text: '.'),
+                    ],
+                  ),
+                ),
+              ),
+              Spacer(),
+              TextButton(
+                onPressed: () {},
+                child: RichText(
+                  text: TextSpan(
+                    style: TextStyle(color: Colors.white),
+                    children: <TextSpan>[
+                      TextSpan(text: 'Don\'t have account? '),
+                      TextSpan(
+                        text: 'Sign up',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
